b0fff29be97d17acd2053cf48202eb3a025c461b
elanglet <30497225+elanglet@users.noreply.github.com>
2022-07-12 08:12:01 +0200

    Delete update-docker-vm.sh

--- update-docker-vm.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash 
-
-sudo apt update &&  \
-sudo apt upgrade -y -qq && \
-sudo apt upgrade -y -qq docker-ce && \
-sudo apt install -y -qq docker-compose-plugin && \
-sudo rm -f /usr/local/bin/docker-compose
